29f14e6d2cd2ba746b346396f24375166af15f88
Fa Kin Yiu, Franklin <lifeaskrystal@hotmail.com>
2024-06-06 00:35:57 -0600

    Added View Order Feature

--- src/types.ts
+++ src/types.ts
@@ -7,13 +7,13 @@ export type User = {
     country: string;
   };
 
-export type MenuItem = {
+  export type MenuItem = {
     _id: string;
     name: string;
     price: number;
   };
   
-export type Restaurant = {
+  export type Restaurant = {
     _id: string;
     user: string;
     restaurantName: string;
@@ -27,7 +27,35 @@ export type Restaurant = {
     lastUpdated: string;
   };
 
-export type RestaurantSearchResponse = {
+  export type OrderStatus =
+  | "placed"
+  | "paid"
+  | "inProgress"
+  | "outForDelivery"
+  | "delivered";
+
+  export type Order = {
+    _id: string;
+    restaurant: Restaurant;
+    user: User;
+    cartItems: {
+      menuItemId: string;
+      name: string;
+      quantity: string;
+    }[];
+    deliveryDetails: {
+      name: string;
+      addressLine1: string;
+      city: string;
+      email: string;
+    };
+    totalAmount: number;
+    status: OrderStatus;
+    createdAt: string;
+    restaurantId: string;
+  };
+
+  export type RestaurantSearchResponse = {
     data: Restaurant[];
     pagination: {
       total: number;
